ed5d2937b73d23f85ca13226a3f48d084e50a160
lhb8106 <lhb8106@naver.com>
2021-11-21 06:46:04 +0900

    [CHORE] - Add MainLopper

--- app/src/main/java/org/sopt/study/woorido/ui/SplashActivity.kt
+++ app/src/main/java/org/sopt/study/woorido/ui/SplashActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.os.Looper
 import org.sopt.study.woorido.MainActivity
 import org.sopt.study.woorido.databinding.ActivityJoinProfileBinding
 import org.sopt.study.woorido.databinding.ActivitySplashBinding
@@ -18,7 +19,7 @@ class SplashActivity : AppCompatActivity() {
         binding = ActivitySplashBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        Handler().postDelayed({
+        Handler(Looper.getMainLooper()).postDelayed({
             val intent = Intent(this, MainActivity::class.java)
             intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION)
             startActivity(intent)
